7d7704c7aff23e54317fe187f440268509336aa0
RiotZanber <149868421+RiotZanber@users.noreply.github.com>
2023-11-21 20:27:24 +0700

    Update ex-1.js

--- ex-1.js
+++ ex-1.js
@@ -1,8 +1,6 @@
 // Exercise #1: Decrease the Student Scores
 let scores = [89, 70, 60, 50, 100, 71];
-for (let i = 0; i<= scores.length; i++){
-  scores[i] -= 10
-}
+
 // Start coding here
 console.log(scores);
 
