4205a0ce8de96d93de4ad9b442ebece71da30afa
Suwinai <suwinai.apionline@gmail.com>
2023-12-05 21:53:02 +0700

    feat: Loop Assignment are finished

--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,13 @@
+
 // Exercise #3: Find a Minimum Number
 let numbers = [100, 20, 3, 1000];
-let minNumber;
-// Start coding here
+let minNumber = numbers[0];
 
+// Start coding here
+for(let i = 1; i < numbers.length; i++){
+    if (numbers[i] < minNumber){
+        minNumber = numbers[i];
+    }
+}
 console.log(minNumber);
+
--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,8 @@
 // Exercise #5: Concatenate the Alphabets
 let companyName = "TechUp";
-
+let numCount = 0;
 // Start coding here
+for (let i of companyName){
+    numCount    ++;
+    console.log(`Number ${numCount} character is ${i}`);
+}
